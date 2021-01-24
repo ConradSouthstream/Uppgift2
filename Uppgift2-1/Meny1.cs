89fcd9bc672acadbcd0e39d6c2b3e4ae23c9d8d0
conny söderström <tripplesouthstream@gmail.com>
2021-01-24 15:10:34 +0100

    Add project files.

---
+++ Uppgift2-1/Meny1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uppgift2
+{
+    class Meny1
+    {
+        public void Menu1()
+        {
+        Console.WriteLine("what");
+        }
+        //if()
+    }
+}
---
+++ Uppgift2-1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Uppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            do
+            {
+                Startmeny();
+                Menyval();
+
+            } while (true);
+
+            
+            
+
+
+        }
+        private static void Startmeny()
+        {
+            Console.WriteLine(
+                "Det här är huvudmenyn, skriv in siffra för ditt val:" +
+                "\n 0 - Gå ur programmet" +
+                "\n 1 - Beräkna biljetter" +
+                "\n 2 - repetativ inskrift" +
+                "\n 3 - Selektivt plock av ord i mening\n"
+                );
+        }
+        private static void Menyval()
+        {
+            switch (Console.ReadLine())
+            {
+                case "0":
+                    Environment.Exit(0);
+                    break;
+                case "1":
+
+                    break;
+
+                case "2":
+
+                    break;
+
+                case "3":
+
+                    break;
+
+                default:
+                    Console.WriteLine("Felaktigt kommando\n");
+                    break;
+            };            
+        }
+    }
+}
